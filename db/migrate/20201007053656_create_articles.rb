a13f794a882cca74a4b6a27507cb73315a99d109
gogutan <50437473+gogutan@users.noreply.github.com>
2020-10-07 14:41:18 +0900

    scaffold article

---
+++ db/migrate/20201007053656_create_articles.rb
@@ -0,0 +1,13 @@
+class CreateArticles < ActiveRecord::Migration[6.0]
+  def change
+    create_table :articles do |t|
+      t.integer :source
+      t.string :title
+      t.string :japanese_title
+      t.integer :words
+      t.integer :level
+
+      t.timestamps
+    end
+  end
+end
