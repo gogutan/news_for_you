b8e061ef8bff70122f6eca449a80e8b224a5171d
gogutan <50437473+gogutan@users.noreply.github.com>
2020-10-21 14:14:56 +0900

    ArticleモデルにURLを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_07_053656) do
+ActiveRecord::Schema.define(version: 2020_10_21_050945) do
 
   create_table "articles", force: :cascade do |t|
     t.integer "source"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_10_07_053656) do
     t.integer "level"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "url"
   end
 
 end
