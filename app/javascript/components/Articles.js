109a080413c5481bbb6290ae2e71b8f0f92896d7
gogutan <50437473+gogutan@users.noreply.github.com>
2020-10-30 19:29:10 +0900

    lintを適用

--- app/javascript/components/Articles.js
+++ app/javascript/components/Articles.js
@@ -1,32 +1,32 @@
-import React from "react"
+import React from 'react'
 
-const range = (start, stop, step) => Array.from({ length: (stop - start) / step + 1}, (_, i) => start + (i * step));
-const scores = range(300, 990, 5);
-const wpms = range(30, 300, 5);
+const range = (start, stop, step) => Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + (i * step))
+const scores = range(300, 990, 5)
+const wpms = range(30, 300, 5)
 
 class Articles extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
       toeic: 600,
       wpm: 100,
       error: null,
       isLoaded: false,
       articles: []
-    };
-    this.handleToeicChange = this.handleToeicChange.bind(this);
-    this.handleWpmChange = this.handleWpmChange.bind(this);
+    }
+    this.handleToeicChange = this.handleToeicChange.bind(this)
+    this.handleWpmChange = this.handleWpmChange.bind(this)
   }
 
-  componentDidMount() {
-    fetch("api/articles")
+  componentDidMount () {
+    fetch('api/articles')
       .then(res => res.json())
       .then(
         (result) => {
           this.setState({
             isLoaded: true,
             articles: result
-          });
+          })
         },
         // 補足：コンポーネント内のバグによる例外を隠蔽しないためにも
         // catch()ブロックの代わりにここでエラーハンドリングすることが重要です
@@ -34,38 +34,38 @@ class Articles extends React.Component {
           this.setState({
             isLoaded: true,
             error
-          });
+          })
         }
       )
   }
 
-  handleToeicChange(event) {
-    this.setState({toeic: event.target.value});
+  handleToeicChange (event) {
+    this.setState({ toeic: event.target.value })
   }
 
-  handleWpmChange(event) {
-    this.setState({wpm: event.target.value});
+  handleWpmChange (event) {
+    this.setState({ wpm: event.target.value })
   }
 
-  render() {
-    const { error, isLoaded, articles } = this.state;
+  render () {
+    const { error, isLoaded, articles } = this.state
     if (error) {
-      return <div>Error: {error.message}</div>;
+      return <div>Error: {error.message}</div>
     } else if (!isLoaded) {
-      return <div>Loading...</div>;
+      return <div>Loading...</div>
     } else {
       return (
         <>
           <label>
             Your TOEIC:
             <select value={this.state.toeic} onChange={this.handleToeicChange}>
-              { scores.map( s => <option key={s.toString()} value={s.toString()}>{s}</option>) }
+              { scores.map(s => <option key={s.toString()} value={s.toString()}>{s}</option>) }
             </select>
           </label>
           <label>
             Your WPM:
             <select value={this.state.wpm} onChange={this.handleWpmChange}>
-            { wpms.map( w => <option key={w.toString()} value={w.toString()}>{w}</option>) }
+            { wpms.map(w => <option key={w.toString()} value={w.toString()}>{w}</option>) }
             </select>
           </label>
           <table>
@@ -93,7 +93,7 @@ class Articles extends React.Component {
             </tbody>
           </table>
         </>
-      );
+      )
     }
   }
 }
